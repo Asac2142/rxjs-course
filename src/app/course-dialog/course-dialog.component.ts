2ab552aeabce1ff377298c7c78972d705c8b4a21
Andres Alban <bethesdas101@gmail.com>
2021-09-23 11:24:41 -0500

    working with rxjs operators

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,59 +1,96 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { concatMap, exhaust, exhaustMap, filter, mergeMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
 })
 export class CourseDialogComponent implements OnInit, AfterViewInit {
+  form: FormGroup;
+  course: Course;
 
-    form: FormGroup;
-    course:Course;
+  @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course) {
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+    this.course = course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+    this.form = this.fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
 
-        this.course = course;
+  }
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+  ngOnInit() {
+    this.withConcatMap();
+    this.withMergeMap();
+  }
 
-    }
+  ngAfterViewInit() {
+    this.withExhaustMap();
+  }
 
-    ngOnInit() {
+  close() {
+    this.dialogRef.close();
+  }
 
+  private withConcatMap(): void {
+    // concatMap, is all about COMPLETION
+    // with concatMap, if we care about the order of emission and subscription of inner observables, we use concatMap
+    // we wait for the first observable to complete, in order to execute the next one
+    // In this example, as we start typing, we first save those changes, it completes giving us back a response from the backend
+    // and after that, we continue to save other stream of changes
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid), // filter is a RxJs opertaror which filters a stream of values given a certain condition
+      concatMap((changes) => this.saveCourses$(changes))
+    ).subscribe();
+  }
 
+  private withMergeMap(): void {
+    // mergeMap, This operator is best used when you wish to flatten an inner observable but want to manually control the number of inner subscriptions.
+    // with mergeMap, we make operations in parallel
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid),
+      mergeMap((changes) => this.saveCourses$(changes))
+    ).subscribe();
+  }
 
-    }
+  private withExhaustMap(): void {
+    // the exhaustMap is an operator that IGNORES new inner observables if the current inner observable is
+    // still on going (still beign processed)
+    // those inner observables ignore are lost, and will only take the next new inner observable after the previous was finished
+    // for this example, this function "withExhaustMap()" will be trigger when the user clicks the save button,
+    // if the user wants to go crazy and start pressing the button like a madman, let say 10 times,
+    // is not going to make 10 http request to the backend, probably, only 2 times, because, those new inner observables
+    // are going to be ignored while the active inner observable is still beign processed, on going
+    // take a look at the Network tab, and see if in reality, we are making like n http calls.... it shouldn't
+    fromEvent(this.saveButton.nativeElement, 'click')
+      .pipe(
+        exhaustMap(() => this.saveCourses$(this.form.value))
+      )
+      .subscribe();
+  }
 
-
-
-    ngAfterViewInit() {
-
-
-    }
-
-
-
-    close() {
-        this.dialogRef.close();
-    }
-
+  private saveCourses$(changes: any) {
+    return fetch(
+      `api/courses/${this.course.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: { 'content-type': 'application-json' }
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,88 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, throwError, timer } from 'rxjs';
+import { catchError, delayWhen, finalize, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
+import { Course } from '../model/course';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  beginnersCourses = [];
+  advanceCourses = [];
+
+  beginnersCourses$: Observable<Course[]>;
+  advanceCourses$: Observable<Course[]>;
+
+  ngOnInit() {
+    // this.imperativeDesign();
+    this.reactiveDesign();
+  }
+
+  private imperativeDesign(): void {
+    const http$ = createHttpObservable('/api/courses');
+    const courses$ = http$.pipe(
+      map((response: any) => Object.values(response.payload))
+    );
+
+    // Imperative way.
+    // With scalation in a project, this is going to be a callback hell
+    // Let's try not to use this in the future
+    courses$.subscribe((courses) => {
+      this.beginnersCourses = courses.filter((course: any) => course.category === 'BEGINNER');
+      this.advanceCourses = courses.filter((course: any) => course.category === 'ADVANCED');
+    });
+  }
+
+  private reactiveDesign(): void {
+    const courses$ = createHttpObservable('/api/courses').pipe(
+      // shareReplay shares this HTTP request, so we don't make multiple calls everytime
+      // we subscribe to that Observable response
+      tap(() => console.log('Http request')), // side effect operator
+      shareReplay(),
+
+      // * 3
+      // using retryWhen, it will retry to make the http request if it fails
+      // retryWhen((error: Observable<any>) => error.pipe(
+      //   delayWhen(() => timer(2000)) //delayWhen, delays the retry operation, // timer, it will wait for 2 secs
+      // ))
 
 
-    constructor() {
+      // for handling errors
+      // Remember to return an observable from the catchError function!
+      catchError((error) => {
+        // * 1
+        // this is a recovery error
+        //  when it catches the error, we want to return a valid observable, in this case, it is an empty course array
+        // instead of an empty array, we could return a array with a default course in it
 
-    }
+        //return of([])
 
-    ngOnInit() {
+        // * 2
+        // throwError operator
+        // this operator returns an observable with the error in it
 
+        return throwError(error)
+      }),
+      finalize(() => console.log('Finalized...'))
+    );
 
+    this.beginnersCourses$ = courses$.pipe(
+      map((response) => Object.values(response['payload'])),
+      map((courses: Course[]) => courses.filter((course) => course.category.toLowerCase() === 'beginner'))
+    );
 
-    }
+    this.advanceCourses$ = courses$.pipe(
+      map((response) => Object.values(response['payload'])),
+      map((courses: Course[]) => courses.filter((course) => course.category.toLowerCase() === 'advanced'))
+    );
 
+    // just for demo purposes
+    // we are subscribing for a 3rd time (1rt and 2nd time subscribes are handle in the HTML template with async pipe)
+    // but we are only making ONE Http request call
+    courses$.subscribe();
+    // if you open the network tab in the Dev tools, you will see ONE Http call /api/courses since we are using ShareReplay
+  }
 }
