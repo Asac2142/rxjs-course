ec98b51826dbf846289501757f9c041f540f03f8
Andres Alban <bethesdas101@gmail.com>
2021-09-23 11:22:52 -0500

    adding util http function

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,20 @@
+import { Observable } from 'rxjs';
 
+export function createHttpObservable(url: string) {
+  return new Observable<any>((observer) => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          observer.error('Error occured with status response' + response.status);
+        }
+      })
+      .then((body) => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch((err) => observer.error(err))
+  });
+}
 
